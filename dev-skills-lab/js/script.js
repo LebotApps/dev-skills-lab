5a3f23b8d25015f7cc97dd708c95c4e84f2897b5
LebotApps <83731206+LebotApps@users.noreply.github.com>
2021-05-20 10:29:53 -0400

    lab upload

---
+++ dev-skills-lab/js/script.js
@@ -0,0 +1,17 @@
+// // $(`body`).append("<h1>MY DEVELOPER SKILLS</h1>");
+
+//jQuery Adding a div
+let  $skillsTable = $("<div id='skillsTable`></div>")
+
+//jQuery adding Table Row Head 
+$('.skillsTable').append('<th> MY DEVELOPER SKILLS </th>')
+
+//jQuery adding color to text and background (Table Header)
+
+$(`.skillsTable`).css("background-color", "rgb(130,186,106").css("color", "white");
+$(`.mySkills`).css("color", "rgb(253,151,30");
+
+// jQuery change color and border color (Button)
+$(`.button`, ).css("color", "rgb(130,186,106");
+$(`.button`).css("border-color", "rgb(130,186,106");
+$(`.button`).css("border-width", "3px");
